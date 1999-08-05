a1ab4cb9d8eb1717c45a20b7f60478817a233b6e
adam <adam>
1999-08-05 17:02:48 +0000

    diff-switches set to -u

--- lib/emacs/init/GNU_Emacs/as-custom.el
+++ lib/emacs/init/GNU_Emacs/as-custom.el
@@ -8,6 +8,7 @@
  '(show-paren-mode t nil (paren))
  '(font-lock-support-mode (quote lazy-lock-mode))
  '(mouse-yank-at-point t)
+ '(diff-switches "-u" t)
  '(lazy-lock-defer-time 0.5)
  '(lazy-lock-stealth-verbose t)
  '(show-paren-ring-bell-on-mismatch nil)
