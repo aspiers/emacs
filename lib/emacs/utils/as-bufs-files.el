e7d9a9d4a3aaecaa49d78ad9d35abcef64cdc1c5
adams <adams>
2003-09-15 12:58:57 +0000

    allow forking to abs

--- lib/emacs/utils/as-bufs-files.el
+++ lib/emacs/utils/as-bufs-files.el
@@ -36,14 +36,24 @@ from the replace is visited via `find-file'."
 (defun as-display-buffer-filename (&optional save-to-clipboard)
   "Displays the current buffer's filename in the minibuffer.
 
-If a prefix argument is given, stores the result in the X selection."
+If a prefix argument is given, stores the result in the kill ring
+and in the X selection for other programs.  If the prefix argument
+is 2, forks the external program abs (must be in $PATH) to convert
+the filename to an absolute one with all symlinks resolved."
   (interactive "P")
   (or buffer-file-name (error "No file associated with this buffer"))
-  (let ((msg buffer-file-name))
+  (let ((fn (cond ((and save-to-clipboard (eq save-to-clipboard 2))
+                   (substring
+                    (shell-command-to-string (concat "abs " buffer-file-name))
+                    0 -1))
+                  (t buffer-file-name))))
     (cond (save-to-clipboard
-           (x-select-text buffer-file-name)
-           (setq msg (concat msg " (stored in X selection)"))))
-    (message msg)))
+           (kill-new fn)
+;; No need for x-select-text since kill-new calls function set
+;; in interprogram-cut-function, which is x-select-text anyway.
+;;         (x-select-text buffer-file-name)
+           (setq fn (concat fn " (stored in kill ring and X selection)"))))
+    (message fn)))
 
 ;;}}}
 ;;{{{ as-destroy-buffer
