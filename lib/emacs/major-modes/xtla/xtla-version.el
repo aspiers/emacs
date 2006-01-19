646621780683fd54bf15a1929d24428876c8f7ce
adam <adam>
2006-01-19 20:35:25 +0000

    remove old xtla

--- lib/emacs/major-modes/xtla/xtla-version.el
+++
@@ -1,4 +0,0 @@
-;; do not edit -- automatically generated file
-;; do not delete -- needed for bug reports
-(defconst tla-version "Matthieu.Moy@imag.fr--public/xtla--main--0.9--patch-65")
-(provide 'xtla-version)
