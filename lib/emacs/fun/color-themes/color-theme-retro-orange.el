72be48a24ad4f60533cb95ee7571c2639ba8a979
adam <adam>
2007-07-05 11:30:56 +0000

    remove ancient color-theme stuff

--- lib/emacs/fun/color-themes/color-theme-retro-orange.el
+++
@@ -1,6 +0,0 @@
-(defun color-theme-retro-orange ()
-  "Plain orange on black faces for those longing for the good old days."
-  (interactive)
-  (color-theme-retro-green "orange" 'color-theme-retro-orange))
-
-(provide 'color-theme-retro-orange)
