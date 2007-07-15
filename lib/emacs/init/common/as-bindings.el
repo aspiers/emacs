8f1a1232d274fb8bea125b5f357eb2094456449a
adam <adam>
2007-07-15 18:57:59 +0000

    more time wasted hacking org.el

--- lib/emacs/init/common/as-bindings.el
+++ lib/emacs/init/common/as-bindings.el
@@ -116,6 +116,7 @@ is already hidden."
 (defvar org-mode-map)
 
 (autoload 'org-todo "org" "org-todo")
+
 (defun org-todo-previous-keyword ()
   "Move to previous TODO keyword in all sets."
   (interactive)
@@ -136,16 +137,77 @@ is already hidden."
   (interactive)
   (org-todo 'nextset))
 
+(autoload 'org-insert-heading "org" "org-insert-heading")
+(autoload 'org-demote-subtree "org" "org-demote-subtree")
+
+(defun org-new-subheading ()
+  "Add a new heading, demoted from the current heading level."
+  (interactive)
+  (org-insert-heading)
+  (org-demote-subtree))
+
+(defun org-new-subheading-todo (arg)
+  "Add a new TODO item, demoted from the current heading level.
+
+The TODO keyword for the new item can be specified by a numeric
+prefix argument, in the same way as `org-todo'.
+
+Otherwise, if `org-subheading-todo-alist' is non-nil, it is used
+to map the new keyword from the current one, and if it is nil,
+the next TODO keyword in the sequence is used, or the first one
+if the current heading does not have one.
+
+This allows a TODO keyword hierarchy to be imposed, e.g.
+if org-subheading-todo-alist is
+
+  '((\"MASTERPLAN\" . \"PROJECT\")
+    (\"PROJECT\" . \"NEXTACTION\")
+    (\"NEXTACTION\" . \"NEXTACTION\"))
+
+then invoking this function four times would yield:
+
+* MASTERPLAN
+** PROJECT
+*** NEXTACTION
+**** NEXTACTION"
+  (interactive "P")
+  (save-excursion
+    (org-back-to-heading)
+    (looking-at org-todo-line-regexp))
+  (let* ((current-keyword (match-string 2))
+         (new-keyword
+          (if arg
+              (nth (1- (prefix-numeric-value arg))
+                   org-todo-keywords-1)
+            (or
+             (and current-keyword
+                  (or (car (assoc current-keyword org-subheading-todo-alist))
+                      (cadr (member current-keyword org-todo-keywords-1))))
+             (car org-todo-keywords-1)))))
+    (org-new-subheading)
+    (insert new-keyword " ")))
+
+(defcustom org-subheading-todo-alist nil
+  "An associative map to help define which TODO keyword should be
+used for new subheadings, depending on the current heading's TODO
+keyword.  See the documentation for `org-new-subheading-todo' for
+an example."
+  :group 'org-todo
+  :type '(alist :key-type   (string :tag "Current heading keyword")
+                :value-type (string :tag "New sub-heading keyword")))
+
 (add-hook 'org-mode-hook
           (lambda ()
-            (define-key org-mode-map [(control left )] 'hide-subtree)
-            (define-key org-mode-map [(control right)] 'as-show-current)
+            (define-key org-mode-map [(control left )]       'hide-subtree)
+            (define-key org-mode-map [(control right)]       'as-show-current)
             (define-key org-mode-map [(control shift left)]  'hide-subtree)
             (define-key org-mode-map [(control shift right)] 'show-subtree)
-            (define-key org-mode-map [(control shift f)] 'org-todo-next-keyword)
-            (define-key org-mode-map [(control shift b)] 'org-todo-previous-keyword)
-            (define-key org-mode-map [(control shift p)] 'org-todo-previous-set)
-            (define-key org-mode-map [(control shift n)] 'org-todo-next-set)
+            (define-key org-mode-map [(control shift f)]     'org-todo-next-keyword)
+            (define-key org-mode-map [(control shift b)]     'org-todo-previous-keyword)
+            (define-key org-mode-map [(control shift p)]     'org-todo-previous-set)
+            (define-key org-mode-map [(control shift n)]     'org-todo-next-set)
+            (define-key org-mode-map [(meta j)]              'org-new-subheading)
+            (define-key org-mode-map [(shift meta j)]        'org-new-subheading-todo)
             ))
 
 ;;}}}
