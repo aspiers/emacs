62c303cc6709488adda86ad87a3914468fc4d2c3
adam <adam>
2007-09-11 09:20:32 +0000

    add comments at the top

--- lib/emacs/utils/smooth-scrolling.el
+++ lib/emacs/utils/smooth-scrolling.el
@@ -1,5 +1,20 @@
-;; See http://www.emacswiki.org/cgi-bin/wiki/SmoothScrolling
-;; for more information on this.
+;; smooth-scrolling.el
+;; $Id$
+;; Adam Spiers <emacs-ss@adamspiers.org>
+;; 
+;; Make emacs scroll smoothly, keeping the point away from the top and
+;; bottom of the current buffer's window in order to keep lines of
+;; context around the point visible as much as possible, whilst
+;; avoiding sudden scroll jumps which are visually confusing.
+;;
+;; This is a nice alternative to all the native scroll-* custom
+;; variables, which unfortunately cannot provide this functionality
+;; perfectly.  `scroll-margin' comes closest, but has some bugs
+;; (e.g. with handling of mouse clicks).  See
+;;
+;;   http://www.emacswiki.org/cgi-bin/wiki/SmoothScrolling
+;;
+;; for the gory details.
 
 (setq scroll-margin 0)
 
