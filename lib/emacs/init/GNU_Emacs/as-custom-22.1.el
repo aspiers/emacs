ca2757c6326bd7ec8f513a8ced925cc0595abd92
adam <adam>
2007-12-29 18:27:46 +0000

    org-mairix

--- lib/emacs/init/GNU_Emacs/as-custom-22.1.el
+++ lib/emacs/init/GNU_Emacs/as-custom-22.1.el
@@ -159,9 +159,11 @@ Reply-To: Adam Spiers <usenet@adamspiers.org>
  '(org-hide-leading-stars t)
  '(org-link-abbrev-alist (quote (("bug" . "https://bugzilla.novell.com/show_bug.cgi?id="))))
  '(org-link-frame-setup (quote ((vm . vm-visit-folder-other-frame) (gnus . gnus-other-frame) (file . find-file))))
+ '(org-mairix-augmented-links nil)
  '(org-mairix-display-hook (quote org-mairix-mutt-display-results))
  '(org-mairix-mutt-display-command "mairix-profile --view novell %search%")
  '(org-mairix-open-command "mairix-profile novell %args% '%search%'")
+ '(org-mairix-threaded-links nil)
  '(org-remember-templates (quote (("new personal NEXT action" 110 "* NEXT %?" "~/roaming/TODO.org" nil) ("new work NEXT action" 78 "* NEXT %?" "~/ifolder/TODO.org" nil) ("NEXT from personal mail" 109 "* NEXT [#B] %?%^{description of personal TODO}
   %[~/.org-mairix-link]" "~/roaming/TODO.org" nil) ("NEXT from work mail" 77 "* NEXT [#B] %?%^{description of work TODO}
   %[~/.org-mairix-link]" "~/ifolder/TODO.org" nil) ("work learning material" 76 "* SOMEDAY %?%[~/.org-mairix-link]	:learning:" "~/ifolder/TODO.org" nil) ("personal task DONE" 100 "* DONE %?%^{work task done}" "~/ifolder/TODO.org" nil) ("work task DONE" 68 "* DONE %?%^{work task done}" "~/ifolder/TODO.org" nil) ("nuisance phone call" 88 "* %T %?%^{description}" "~/roaming/notes/NuisanceCalls.org" nil))))
