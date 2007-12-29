b15b1224127925cf9bbaeeeac9ebd0561d0ae2ad
adam <adam>
2007-12-29 18:09:26 +0000

    as-gtd now uses org-mairix-mutt stuff

--- lib/emacs/utils/as-gtd.el
+++ lib/emacs/utils/as-gtd.el
@@ -1,16 +1,12 @@
 ;; Things to help me with Getting Things Done.
 
-(defvar as-mairix-links-clipboard "~/.org-mairix-link"
-  "Pseudo-clipboard file where mairix URLs get copied to.")
+(require 'org-mairix)
 
-(defvar as-mairix-results-folder "~/mail/mairix"
-  "Folder where mairix writes results.")
-
-(defvar as-mairix-link-viewer-command "mairix '%search%' && xterm -title 'mairix search: %search%' -e 'unset COLUMNS; mutt -F ~/.mutt/work -f %folder% -e \"push <display-message>\"' &"
-  "Command to view messages linked to by 'mairix://...' links.")
+(defvar as-mairix-links-clipboard "~/.clip-mairix"
+  "[Deprecated] Pseudo-clipboard file where mairix URLs get copied to.")
 
 (defun as-mairix-yank-links ()
-  "Yank from file defined by `as-mairix-links-clipboard'."
+  "[Deprecated] Yank from file defined by `as-mairix-links-clipboard'."
   (interactive)
   (let ((bytes (cadr (insert-file-contents
                       (expand-file-name as-mairix-links-clipboard)))))
@@ -21,9 +17,9 @@
           (delete-char 1)))))
 
 (defun as-mairix-search-at-point ()
-  "Return the start and end points of a mairix link at the current
-point.  The result is a paired list of character positions for a
-mairix link located at the current point in the current buffer."
+  "[Deprecated] Return the start and end points of a mairix link at
+the current point.  The result is a paired list of character positions
+for a mairix link located at the current point in the current buffer."
   (save-excursion
     (if (looking-at "<mairix://")
         (forward-char 1)
@@ -33,16 +29,14 @@ mairix link located at the current point in the current buffer."
       nil)))
 
 (defun as-mairix-view-link-at-point (show-async-buf)
-  "View the 'mairix://...' link at the point using the shell command
-defined by `as-mairix-link-viewer-command'."
+  "[Deprecated] View the 'mairix://...' link at the point using the
+shell command defined by `org-mairix-mutt-display-command'."
   (interactive "P")
   (let ((message-id (as-mairix-search-at-point)))
     (or message-id (error "No mairix URL found at point"))
-    (let ((cmd as-mairix-link-viewer-command))
+    (let ((cmd org-mairix-mutt-display-command))
       (while (string-match "%search%" cmd)
         (setq cmd (replace-match message-id 'fixedcase 'literal cmd)))
-      (while (string-match "%folder%" cmd)
-        (setq cmd (replace-match as-mairix-results-folder 'fixedcase 'literal cmd)))
       ;; By default, async `shell-command' invocations display the temp
       ;; buffer, which is annoying here.  We choose a deterministic
       ;; buffer name so we can hide it again immediately.
