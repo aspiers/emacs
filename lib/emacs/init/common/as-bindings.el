52e59908f59320b60ab75b465766ad33ad200353
adam <adam>
2008-01-22 19:54:50 +0000

    C-c C-? => org-occur-in-agenda-files

--- lib/emacs/init/common/as-bindings.el
+++ lib/emacs/init/common/as-bindings.el
@@ -602,6 +602,8 @@ consistent landing spot."
 (autoload 'set-any-variable "set-any-var" "set-any-variable" t)
 (global-set-key "\C-cv"   'set-any-variable)
 (global-set-key "\C-c+"   'make-directory)
+(autoload 'org-occur-in-agenda-files "org" nil t)
+(global-set-key [(control c) (control \?)] 'org-occur-in-agenda-files)
 
 ;;}}}
 ;;{{{ Function keys f5--f9 (no modifiers)
