5adc3a6509deafe7b80f16a549a91ff041cd589c
adam <adam>
2010-10-10 21:00:27 +0000

    add cucumber.el support

--- lib/emacs/init/common/as-init.el
+++ lib/emacs/init/common/as-init.el
@@ -328,6 +328,27 @@ FIXME: needs to tweak align-*-modes too."
 (add-to-list 'auto-mode-alist '("\\(\\.rb\\|\\.erb\\|\\.rjs\\|Rakefile\\)\\'" . ruby-mode))
 (autoload 'ruby-mode "ruby-mode" "ruby-mode" t)
 
+;;{{{ rcov.el
+
+;; corrections for rcov.el, although it kinda looks lame - no red/green overlaying
+
+(autoload 'rcov "rcov" "rcov" t)
+
+(defvar rcov-command-line "rake test:units:rcov RCOVOPTS='--gcc --no-html'"
+  "Rcov command line to find uncovered code.
+It is good to use rcov with Rake because it `cd's appropriate directory.
+`--gcc' option is strongly recommended because `rcov' uses compilation-mode.")
+(defvar rcovsave-command-line "rake rcov RCOVOPTS='--gcc --no-html --save=coverage.info'"
+  "Rcov command line to save coverage status. See also `rcov-command-line'.")
+(defvar rcovdiff-command-line "rake rcov RCOVOPTS='-D --gcc --no-html'"
+  "Rcov command line to find new uncovered code. See also `rcov-command-line'.")
+
+;;}}}
+
+(autoload 'rcov-buffer "rcov-overlay" "rcov-overlay" t)
+;; (global-set-key (kbd "C-c C-r")   'rcov-buffer)
+(autoload 'autotest "autotest" "autotest" t)
+
 ;;}}}
 ;;{{{ Shell-script
 
@@ -559,6 +580,17 @@ C-style indentation, use cssm-c-style-indenter.")
 (add-to-list 'auto-mode-alist '("\\.css\\'" . css-mode))
 
 ;;}}}
+
+;;{{{ Gherkin (Cucumber)
+
+(add-to-list 'load-path "~/lib/emacs/major-modes/cucumber")
+;(setq feature-default-language "en")
+;(setq feature-default-i18n-file "/path/to/gherkin/gem/i18n.yml")
+(require 'feature-mode)
+(add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
+
+;;}}}
+
 
 ;;}}}
 ;;{{{ Configuration languages
