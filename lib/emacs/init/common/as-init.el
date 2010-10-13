e2e2bb67248b5aee580e14da3b022b163c1f005a
adam <adam>
2010-10-13 14:38:21 +0000

    import pomodoro config from worg

--- lib/emacs/init/common/as-init.el
+++ lib/emacs/init/common/as-init.el
@@ -921,6 +921,21 @@ then invoking this function four times would yield:
 
 (autoload 'bzg/org-annotation-helper "org-annotation-helper" nil t)
 
+;;{{{ pomodoro
+
+;; http://orgmode.org/worg/org-gtd-etc.php
+
+(add-to-list 'org-modules 'org-timer)
+(setq org-timer-default-timer 25)
+
+;; Modify the org-clock-in so that a timer is started with the default
+;; value except if a timer is already started :
+(add-hook 'org-clock-in-hook '(lambda () 
+      (if (not org-timer-current-timer) 
+      (org-timer-set-timer '(16)))))
+
+;;}}}
+
 ;;}}}
 ;;{{{ remember-mode with org-mode
 
