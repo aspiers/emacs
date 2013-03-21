51caf36208d6cc370bdeba6356b59ed08da4ced9
Adam Spiers <dotfiles@adamspiers.org>
2013-03-21 23:25:59 +0000

    add xml-rpc

---
+++ .emacs.d/elpa/xml-rpc-1.6.8/xml-rpc-autoloads.el
@@ -0,0 +1,18 @@
+;;; xml-rpc-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("xml-rpc-pkg.el" "xml-rpc.el") (20806
+;;;;;;  26788 148626))
+
+;;;***
+
+(provide 'xml-rpc-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; xml-rpc-autoloads.el ends here
---
+++ .emacs.d/elpa/xml-rpc-1.6.8/xml-rpc-pkg.el
@@ -0,0 +1,1 @@
+(define-package "xml-rpc" "1.6.8" "An elisp implementation of clientside XML-RPC" (quote nil))
