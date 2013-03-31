fa76cfe102651de096281e8ea0afc53ca82ccb09
Adam Spiers <dotfiles@adamspiers.org>
2013-03-31 12:03:36 +0100

    edit-server-htmlize

--- lib/emacs/init/common/as-init.el
+++ lib/emacs/init/common/as-init.el
@@ -160,6 +160,11 @@
     (edit-server-stop)
     (edit-server-start)))
 
+(autoload 'edit-server-maybe-dehtmlize-buffer "edit-server-htmlize" "edit-server-htmlize" t)
+(autoload 'edit-server-maybe-htmlize-buffer   "edit-server-htmlize" "edit-server-htmlize" t)
+(add-hook 'edit-server-start-hook 'edit-server-maybe-dehtmlize-buffer)
+(add-hook 'edit-server-done-hook  'edit-server-maybe-htmlize-buffer)
+
 ;;}}}
 ;;{{{ Diary, appointments
 
