1f099a5dfcfc941d52578348f111acfbf0c9ab1d
Adam Spiers <dotfiles@adamspiers.org>
2013-12-07 14:37:09 +0000

    remove useless minor mode lighters

---
+++ .emacs.d/init.d/90-mode-lighters.el
@@ -0,0 +1,8 @@
+;; http://hbin.me/blog/2012/08/26/clean-the-emacs-mode-line/
+
+(defun hbin-remove-mm-lighter (mm)
+  "Remove minor lighter from the mode line."
+  (setcar (cdr (assq mm minor-mode-alist)) nil))
+
+(hbin-remove-mm-lighter 'global-whitespace-mode)
+(hbin-remove-mm-lighter 'whitespace-mode)
