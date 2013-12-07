19d38e7d4d1ecb363dcb7bad201e7b765434b68b
Adam Spiers <dotfiles@adamspiers.org>
2013-12-07 14:43:04 +0000

    start trying to figure out load order for as-custom

--- .emacs.d/init.d/50-custom.el
+++ .emacs.d/init.d/50-custom.el
@@ -1,3 +1,11 @@
+;; Reasons for loading this before el-get packages:
+;;
+;;   * org-disputed-keys and org-replace-disputed-keys won't work
+;;     unless set before org.el is loaded for the first time
+;;
+;; Reasons for loading this before el-get packages:
+;;
+
 (cond
  ((boundp 'running-xemacs)
   ;; XEmacs automatically saved settings go here:
