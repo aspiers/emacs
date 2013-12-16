566fb10cb699a24be5f7e891b0fdfaf7435db947
Adam Spiers <dotfiles@adamspiers.org>
2013-12-16 14:22:57 +0000

    paredit rocks

--- .emacs.d/init.d/as-prog-langs.el
+++ .emacs.d/init.d/as-prog-langs.el
@@ -175,8 +175,12 @@ It is good to use rcov with Rake because it `cd's appropriate directory.
   :commands enable-paredit-mode
   :init
   (progn
-    (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
-    (add-hook 'lisp-mode-hook       'enable-paredit-mode)))
+    (add-hook 'emacs-lisp-mode-hook       'enable-paredit-mode)
+    (add-hook 'lisp-mode-hook             'enable-paredit-mode)
+    (add-hook 'ielm-mode-hook             'enable-paredit-mode)
+    (add-hook 'scheme-mode-hook           'enable-paredit-mode)
+    (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
+    (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)))
 
 ;;{{{ macrostep
 
