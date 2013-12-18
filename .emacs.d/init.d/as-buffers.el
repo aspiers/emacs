d047f932ee082c600c2c1bdb6e49046ad4d80230
Adam Spiers <dotfiles@adamspiers.org>
2013-12-18 12:35:57 +0000

    switch as-files to bind-key and use-package

--- .emacs.d/init.d/as-buffers.el
+++ .emacs.d/init.d/as-buffers.el
@@ -2,29 +2,22 @@
 
 ;; Set C-x C-b to buffer-menu rather than list-buffers so that the
 ;; point automatically gets put in the buffer menu.
-(global-set-key [(control x)(control b)] 'buffer-menu)
+(bind-key "C-x C-b" 'buffer-menu)
 
 ;; But if bs-show is available, choose that cos it's much nicer.
-(if (functionp 'bs-show)
-     (global-set-key [(control x)(control b)]
-                     (lambda (arg)
-                       (interactive "P")
-                       (bs-show arg)
-                       (let ((inhibit-read-only t))
-                         (save-excursion
-                           (goto-char (point-max))
-                           (insert "\n"))))))
+(use-package bs-show
+  :bind ("C-x C-b" . bs-show))
 
-(global-set-key [(control x) K] 'as-destroy-buffer)
-(global-set-key [(control !)]             'ido-switch-buffer)
-(global-set-key [(control =)]             'switch-to-prev-buffer)
-(global-set-key [(control +)]             'switch-to-next-buffer)
-(global-set-key "\C-cr"   'revert-buffer)
-(global-set-key "\C-cb"   'bury-buffer)
-(global-set-key [(control \;)]            'bury-buffer)
-(global-set-key "\C-cB"   'as-bounce-buffer)
-(global-set-key [(control x) (I)]         'insert-buffer)
-(global-set-key [(f6)] 'as-bounce-buffer)
-(global-set-key "\C-cn"   'as-display-buffer-filename)
+(bind-key "C-x K" 'as-destroy-buffer)
+(bind-key "C-!"   'ido-switch-buffer)
+(bind-key "C-="   'switch-to-prev-buffer)
+(bind-key "C-+"   'switch-to-next-buffer)
+(bind-key "C-c r" 'revert-buffer)
+(bind-key "C-c b" 'bury-buffer)
+(bind-key "C-;"   'bury-buffer)
+(bind-key "C-c B" 'as-bounce-buffer)
+(bind-key "C-x I" 'insert-buffer)
+(bind-key "<F6>"  'as-bounce-buffer)
+(bind-key "C-c n" 'as-display-buffer-filename)
 
 (provide 'as-buffers)
