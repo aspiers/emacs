03f1c4abdac13e876cca7f7868c2fbfbb00660b4
Adam Spiers <dotfiles@adamspiers.org>
2014-01-04 12:46:30 +0000

    explain M-mouse-4/5 and switch them to bind-key

--- .emacs.d/init.d/as-mouse.el
+++ .emacs.d/init.d/as-mouse.el
@@ -3,9 +3,11 @@
        (load "mwheel" t)
        (mwheel-install)))
 
-(and window-system
-     (global-set-key [(M-mouse-4)] 'raise-frame)
-     (global-set-key [(M-mouse-5)] 'lower-frame))
+;; This is presumably only useful when my normal window manager
+;; mouse bindings and/or Meta key binding are not in effect.
+(when window-system
+  (bind-key "M-mouse-4" 'lower-frame)
+  (bind-key "M-mouse-5" 'raise-frame))
 
 (global-set-key [(control meta ??)]       'bn-make-region-into-secondary)
 (global-set-key [(control meta T)]        'bn-exchange-region-and-secondary)
