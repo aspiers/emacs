bd453f460fb407fdbd86047d1e35a583f87d8d1f
Adam Spiers <dotfiles@adamspiers.org>
2014-01-04 16:47:06 +0000

    add M-o to guide-key

--- lib/emacs/init/GNU_Emacs/as-custom-24.2.el
+++ lib/emacs/init/GNU_Emacs/as-custom-24.2.el
@@ -1365,7 +1365,7 @@
  '(guide-key-mode t)
  '(guide-key/guide-key-sequence
    (quote
-    ("C-x 4" "C-x r" "C-x v" "C-x 8" "C-x p" "C-c i" "C-c g" "C-c m" "C-c t" "C-c w" "C-c j" "zp" "pz" "<key-chord>"
+    ("C-x 4" "C-x r" "C-x v" "C-x 8" "C-x p" "C-c i" "C-c g" "C-c m" "C-c t" "C-c w" "C-c j" "M-o" "zp" "pz" "<key-chord>"
      (dired-mode "%" "*" "C-t"))))
  '(guide-key/popup-window-position (quote bottom))
  '(guide-key/recursive-key-sequence-flag t)
