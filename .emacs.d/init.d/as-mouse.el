6bd371144947cd43797bba3e8c0c500e3b8389ad
Adam Spiers <dotfiles@adamspiers.org>
2014-01-04 12:47:31 +0000

    change mouse bindings for secondary selection
    and use bind-key.  Standard bindings get captured by my wm setup.

--- .emacs.d/init.d/as-mouse.el
+++ .emacs.d/init.d/as-mouse.el
@@ -9,5 +9,10 @@
   (bind-key "M-mouse-4" 'lower-frame)
   (bind-key "M-mouse-5" 'raise-frame))
 
-(global-set-key [(control meta ??)]       'bn-make-region-into-secondary)
-(global-set-key [(control meta T)]        'bn-exchange-region-and-secondary)
+(bind-key "C-S-<mouse-1>"      'mouse-start-secondary)
+(bind-key "C-S-<drag-mouse-1>" 'mouse-set-secondary)
+(bind-key "C-S-<down-mouse-1>" 'mouse-drag-secondary)
+(bind-key "C-S-<mouse-3>"      'mouse-secondary-save-then-kill)
+(bind-key "C-S-<mouse-2>"      'mouse-yank-secondary)
+(bind-key "C-M-?"              'bn-make-region-into-secondary)
+(bind-key "C-M-T"              'bn-exchange-region-and-secondary)
