41c9e4b4e18ad25a62139a493da12683aff15576
Adam Spiers <dotfiles@adamspiers.org>
2014-04-26 22:56:27 +0200

    phi-search only suitable for multiple-cursors at the moment

--- .emacs.d/init.d/as-phi-search.el
+++ .emacs.d/init.d/as-phi-search.el
@@ -1,6 +1,8 @@
 (use-package phi-search
-  :bind (("C-s" . phi-search)
-         ("C-r" . phi-search-backward))
-  :init (require 'phi-search-mc))
+  :bind (("C-c m C-s" . phi-search)
+         ("C-c m C-r" . phi-search-backward))
+  :init (progn
+          (require 'phi-search-mc)
+          (define-key phi-search-default-map (kbd "C-g") 'phi-search-abort)))
 
 (provide 'as-phi-search)
