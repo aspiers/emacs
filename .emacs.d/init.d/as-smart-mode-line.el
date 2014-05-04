50dd5bf55f3e556b3913d89d31edd7067fe0682e
Adam Spiers <dotfiles@adamspiers.org>
2014-05-04 21:29:44 +0100

    only apply smart-mode-line workaround when needed

--- .emacs.d/init.d/as-smart-mode-line.el
+++ .emacs.d/init.d/as-smart-mode-line.el
@@ -4,7 +4,9 @@
   :config
   (progn
     (sml/setup)
-    (unless (member mode-line-front-space mode-line-format)
-      (setq-default mode-line-format (cons mode-line-front-space mode-line-format)))))
+    ;; https://github.com/Bruce-Connor/smart-mode-line/issues/51
+    (when (version<= emacs-version "24.3")
+      (unless (member mode-line-front-space mode-line-format)
+        (setq-default mode-line-format (cons mode-line-front-space mode-line-format))))))
 
 (provide 'as-smart-mode-line)
