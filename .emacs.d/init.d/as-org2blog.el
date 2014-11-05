c46e32072c1d5863cf9fbd87f2b44162ad74afe4
Adam Spiers <dotfiles@adamspiers.org>
2014-11-06 00:00:42 +0100

    fix netrc evaluation

--- .emacs.d/init.d/as-org2blog.el
+++ .emacs.d/init.d/as-org2blog.el
@@ -2,10 +2,10 @@
 
 (setq blog (netrc-machine (netrc-parse "~/.netrc") "blog.adamspiers.org" t))
 (setq org2blog/wp-blog-alist
-      '(("my-blog"
+      `(("my-blog"
          :url "http://blog.adamspiers.org/xmlrpc.php"
-         :username (netrc-get blog "login")
-         :password (netrc-get blog "password"))))
+         :username ,(netrc-get blog "login")
+         :password ,(netrc-get blog "password"))))
 
 (require 'as-require)
 (when (as-check-feature-loaded 'org2blog)
