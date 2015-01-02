856fc94a10f85a9e472ef7f27adb3d1666e0d0c8
Adam Spiers <dotfiles@adamspiers.org>
2015-01-02 00:01:53 +0000

    as-quelpa

---
+++ .emacs.d/init.d/as-quelpa.el
@@ -0,0 +1,9 @@
+(package-initialize)
+
+(if (require 'quelpa nil t)
+    (quelpa-self-upgrade)
+  (with-temp-buffer
+    (url-insert-file-contents "https://raw.github.com/quelpa/quelpa/master/bootstrap.el")
+    (eval-buffer)))
+
+(provide 'as-quelpa)
