994c69aa34ff26ab5f84eaafd95191fb080c7891
Adam Spiers <dotfiles@adamspiers.org>
2015-03-23 10:32:56 +0000

    quieten as-quelpa

--- .emacs.d/init.d/as-quelpa.el
+++ .emacs.d/init.d/as-quelpa.el
@@ -1,8 +1,8 @@
 (package-initialize)
 
 (if (require 'quelpa nil t)
+    ;; Skipping buggy quelpa-self-upgrade
     ;;(quelpa-self-upgrade)
-    (message "Skipping buggy quelpa-self-upgrade")
   (with-temp-buffer
     (url-insert-file-contents "https://raw.github.com/quelpa/quelpa/master/bootstrap.el")
     (eval-buffer)))
