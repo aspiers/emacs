8efa5c386ff96686c4a0f47c440a11d4329acd1f
Adam Spiers <dotfiles@adamspiers.org>
2015-08-13 16:59:54 +0100

    setup magit-repository-directories from projectile

--- .emacs.d/init.d/as-magit.el
+++ .emacs.d/init.d/as-magit.el
@@ -17,7 +17,23 @@
         ;; when C-d is hit during ido mode (see ido-common-completion-map).
         (setq fallback 'magit-status)
         (setq ido-exit 'fallback)
-        (exit-minibuffer)))))
+        (exit-minibuffer)))
+
+    ;; Improved and updated version of this nice idea from
+    ;; http://iqbalansari.github.io/blog/2014/02/22/switching-repositories-with-magit/
+    (eval-after-load 'projectile
+      (progn
+        (require 'tramp)
+        (setq magit-repository-directories
+              (mapcar (lambda (dir) (directory-file-name dir))
+                      ;; remove tramp and non-git projects
+                      (remove-if
+                       (lambda (project)
+                         (or
+                          (tramp-tramp-file-p project)
+                          (not (file-directory-p (concat project "/.git")))))
+                       (projectile-relevant-known-projects))))
+        (setq magit-repo-dirs-depth 1)))))
 
 (use-package magit-topgit)
 
