6fe0e3665307f642219aeaf579d2ff20b2e7183d
Adam Spiers <dotfiles@adamspiers.org>
2015-09-17 20:39:06 +0100

    more guide-key sequences

--- lib/emacs/init/GNU_Emacs/as-custom-24.5.el
+++ lib/emacs/init/GNU_Emacs/as-custom-24.5.el
@@ -1382,7 +1382,7 @@
  '(guide-key-mode t)
  '(guide-key/guide-key-sequence
    (quote
-    ("C-x 4" "C-x r" "C-x v" "C-x 8" "C-x p" "C-c" "C-c i" "C-c g" "C-c m" "C-c t" "C-c C-v" "C-c w" "C-c j" "C-S-SPC" "M-o" "zp" "pz" "<key-chord>"
+    ("C-x 4" "C-x C-k" "C-x r" "C-x v" "C-x 8" "C-x p" "C-c" "C-c i" "C-c g" "C-c m" "C-c t" "C-c C-v" "C-c w" "C-c j" "C-h" "C-S-SPC" "M-o" "zp" "pz" "<key-chord>"
      (dired-mode "%" "*" "C-t"))))
  '(guide-key/popup-window-position (quote bottom))
  '(guide-key/recursive-key-sequence-flag t)
