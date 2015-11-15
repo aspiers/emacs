b13060a9cade44754adfc766dc0272a1ab2f11af
Adam Spiers <dotfiles@adamspiers.org>
2015-11-15 22:29:33 +0000

    move client/server stuff to as-server

--- .emacs.d/init.d/as-misc.el
+++ .emacs.d/init.d/as-misc.el
@@ -45,48 +45,6 @@
   :if (not (as-quick-startup)))
 
 ;;}}}
-;;{{{ client/server mode
-
-(use-package server
-  :commands (server-start server-force-delete)
-  :init
-  (progn
-    (defun ss ()
-      "Abbreviation for `server-start'."
-      (interactive)
-      (server-start))
-    (defun server-restart (interactive)
-      "Equivalent to `server-force-delete' followed by `server-start'."
-      (server-force-delete)
-      (server-start))
-    (unless (as-quick-startup)
-      (condition-case err
-          (server-start)
-        (file-error (message "%s" (error-message-string err)))))))
-
-(use-package edit-server
-  :commands (edit-server-start edit-server-stop)
-  :defer 10
-  :config
-  (unless (as-quick-startup)
-    (condition-case err
-        (edit-server-start)
-      (file-error (message "%s" (error-message-string err))))
-    (defun edit-server-restart (interactive)
-      "Equivalent to `edit-server-stop' followed by `edit-server-start'."
-      (edit-server-stop)
-      (edit-server-start))))
-
-(use-package edit-server-htmlize
-  :commands (edit-server-maybe-dehtmlize-buffer
-             edit-server-maybe-htmlize-buffer)
-  :init
-  ;; FIXME: use req-package
-  (when (featurep 'edit-server)
-    (add-hook 'edit-server-start-hook 'edit-server-maybe-dehtmlize-buffer)
-    (add-hook 'edit-server-done-hook  'edit-server-maybe-htmlize-buffer)))
-
-;;}}}
 ;;{{{ Diary, appointments
 
 ;; suspect I don't need this any more
