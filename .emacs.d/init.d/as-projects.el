6656bcf6db41c5052655ab658b9445680ef707ad
Adam Spiers <dotfiles@adamspiers.org>
2015-11-20 18:18:09 +0000

    ditch unnecessary progn

--- .emacs.d/init.d/as-projects.el
+++ .emacs.d/init.d/as-projects.el
@@ -1,30 +1,28 @@
 (custom-set-variables '(projectile-keymap-prefix "^Xp"))
 (use-package projectile
   :config
-  (progn
+  ;; https://github.com/bbatsov/projectile/issues/287
+  (defvar projectile-prefix-map
+    (lookup-key projectile-mode-map (kbd "C-c p"))
+    "The keymap which Projectile typically binds to C-c p.")
+  (bind-key "C-x p" projectile-prefix-map)
+  (global-set-key (kbd "C-c p") 'previous-line)
+  (local-set-key (kbd "C-c p") 'previous-line)
+  (global-set-key [(control ?c) ?p] 'previous-line)
+  (define-key mode-specific-map (kbd "p") nil)
+  (bind-key "C-c p" 'as-copy-previous-line-suffix)
+  (require 'as-key-chord)
 
-    ;; https://github.com/bbatsov/projectile/issues/287
-    (defvar projectile-prefix-map
-      (lookup-key projectile-mode-map (kbd "C-c p"))
-      "The keymap which Projectile typically binds to C-c p.")
-    (bind-key "C-x p" projectile-prefix-map)
-    (global-set-key (kbd "C-c p") 'previous-line)
-    (local-set-key (kbd "C-c p") 'previous-line)
-    (global-set-key [(control ?c) ?p] 'previous-line)
-    (define-key mode-specific-map (kbd "p") nil)
-    (bind-key "C-c p" 'as-copy-previous-line-suffix)
-    (require 'as-key-chord)
+  ;; Not needed since guide-key/recursive-key-sequence-flag is set:
+  ;; (setq guide-key/guide-key-sequence
+  ;;       '("<key-chord> z p" "<key-chord> p z"))
+  (key-chord-define-global "zp" projectile-prefix-map)
 
-    ;; Not needed since guide-key/recursive-key-sequence-flag is set:
-    ;; (setq guide-key/guide-key-sequence
-    ;;       '("<key-chord> z p" "<key-chord> p z"))
-    (key-chord-define-global "zp" projectile-prefix-map)
-
-    (key-chord-define-global "ZG" 'projectile-grep)
-    (key-chord-define-global "zm" 'projectile-commander)
-    (key-chord-define-global "zs" 'projectile-switch-project)
-    (key-chord-define-global "zf" 'projectile-find-file)
-    (key-chord-define-global "zb" 'projectile-switch-to-buffer)))
+  (key-chord-define-global "ZG" 'projectile-grep)
+  (key-chord-define-global "zm" 'projectile-commander)
+  (key-chord-define-global "zs" 'projectile-switch-project)
+  (key-chord-define-global "zf" 'projectile-find-file)
+  (key-chord-define-global "zb" 'projectile-switch-to-buffer))
 
 (require 'as-find-file-in-dir)
 (define-find-file-in-dir-function as-find-my-mrconfig
