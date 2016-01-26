57f6358982a62292584b9e377c72bbb35de1734c
Adam Spiers <dotfiles@adamspiers.org>
2016-01-26 09:36:36 +0000

    fix color-theme stuff for req-package

--- .emacs.d/init.d/as-ui.el
+++ .emacs.d/init.d/as-ui.el
@@ -37,13 +37,6 @@
 
 ;;}}}
 
-(req-package color-theme)
-(req-package color-theme-pastels-on-dark
-  ;; FIXME: use req-package
-  :if (featurep 'color-theme)
-  :config
-  (color-theme-pastels-on-dark))
-
 (req-package rainbow-delimiters
   :config (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
 
