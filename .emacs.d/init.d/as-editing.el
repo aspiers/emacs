23a22af991b35fcc7a96ac12847e1b8723d4c5c2
Adam Spiers <dotfiles@adamspiers.org>
2017-10-03 20:06:19 +0100

    get closer to finishing as-format-as-flowed-text

--- .emacs.d/init.d/as-editing.el
+++ .emacs.d/init.d/as-editing.el
@@ -28,15 +28,32 @@ The list marked is the one that contains point or follows point."
 
 (defun as-format-as-flowed-text ()
   "Format the buffer as flowed text according to RFC 2646.
-This simply ensures that appropriate lines should be terminated
-with a single space.  Operates on the current region if active,
-otherwise on the whole buffer."
+This ensures that appropriate lines should be terminated with a
+single space, and that \"> \" quoting prefixes are replaced with
+\">\".  Operates on the current region if active, otherwise on
+the whole buffer."
   (interactive)
-  (save-excursion
-    (or (use-region-p) (goto-char (point-min)))
-    (while (re-search-forward "^\\(>+ ?\\)?\\S-.*\\S-$"
-                              (and (use-region-p) (point-max)) t)
-      (replace-match "\\& " t))))
+  (let ((start (if (use-region-p) (region-beginning) (point-min)))
+        (end (if (use-region-p) (region-end) (point-max))))
+    (save-excursion
+      (goto-char start)
+      ;; Ensure appropriate lines end with a space
+      (while (re-search-forward "^\\(>+ ?\\)?\\S-.*\\S-$" end t)
+        (replace-match "\\& " t))
+
+      ;; Replace "> " quoting prefixes with ">"
+      (goto-char start)
+      (let ((eol)
+            (eolm (make-marker)))
+        (while (setq eol (re-search-forward "^>.*" end t))
+          (set-marker eolm eol)
+          (goto-char (match-beginning 0))
+          (while (looking-at ">")
+            (if (looking-at "> ")
+                (replace-match ">")
+              (forward-char)))
+          (goto-char (marker-position eolm)))))))
+
 (bind-key "M-s M-m" 'as-format-as-flowed-text)
 
 (req-package bn-kill-stuff
