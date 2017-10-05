cfa1b31dec233d3959e1478be161fcd90a01f44d
Adam Spiers <dotfiles@adamspiers.org>
2017-10-05 12:31:05 +0100

    move w3m stuff to as-web.el

--- .emacs.d/init.d/as-comms.el
+++ .emacs.d/init.d/as-comms.el
@@ -19,35 +19,6 @@
 (add-to-list 'auto-mode-alist '("\\.crm$" . crm114-mode))
 
 ;;}}}
-;;{{{ w3m-mode
-
-;; Pull in autoloads
-(require 'w3m-load "w3m-load" t)
-(autoload 'w3m-find-file "w3m" nil t)
-
-(eval-when-compile (require 'dired))
-(add-hook 'dired-mode-hook
-          (lambda ()
-            (define-key dired-mode-map "\C-xm" 'dired-w3m-find-file)))
-
-(autoload 'w3m-find-file "w3m")
-(autoload 'dired-get-filename "dired")
-(defun dired-w3m-find-file ()
-  (interactive)
-  (let ((file (dired-get-filename)))
-    (if (y-or-n-p (format "Open 'w3m' %s " (file-name-nondirectory file)))
-        (w3m-find-file file))))
-
-(defun mhj-w3m-browse-current-buffer ()
-  (interactive)
-  (let ((filename (concat (make-temp-file "w3m-" nil) ".html")))
-    (unwind-protect
-        (progn
-          (write-region (point-min) (point-max) filename)
-          (w3m-find-file filename))
-      (delete-file filename))))
-
-;;}}}
 ;;{{{ ispell
 
 (bind-key "C-$"   'ispell-complete-word)
