b00f16181088682daf5012ed628a062214274900
Adam Spiers <dotfiles@adamspiers.org>
2017-10-05 12:30:22 +0100

    extract blogging from as-comms.el to as-blog.el

---
+++ .emacs.d/init.d/as-blog.el
@@ -0,0 +1,16 @@
+(add-to-list 'auto-mode-alist
+             '("blog\\.adamspiers\\.org\\..*\\.txt\\'" . web-mode))
+
+(req-package org2blog
+  :requires org
+  :config
+  (add-hook 'org-mode-hook
+            (lambda ()
+              (and (buffer-file-name)
+                   (string-match "\\.o2b$" (buffer-file-name))
+                   (org2blog/wp-mode)))))
+
+(bind-key "C-c w l"  'org2blog/wp-login)
+(bind-key "C-c w n"  'org2blog/wp-new-entry)
+
+(provide 'as-blog)
--- .emacs.d/init.d/as-comms.el
+++ .emacs.d/init.d/as-comms.el
@@ -48,27 +48,6 @@
       (delete-file filename))))
 
 ;;}}}
-;;{{{ wordpress
-
-(add-to-list 'auto-mode-alist
-             '("blog\\.adamspiers\\.org\\..*\\.txt\\'" . web-mode))
-
-;;}}}
-;;{{{ org2blog
-
-(req-package org2blog
-  :requires org
-  :config
-  (add-hook 'org-mode-hook
-            (lambda ()
-              (and (buffer-file-name)
-                   (string-match "\\.o2b$" (buffer-file-name))
-                   (org2blog/wp-mode)))))
-
-(bind-key "C-c w l"  'org2blog/wp-login)
-(bind-key "C-c w n"  'org2blog/wp-new-entry)
-
-;;}}}
 ;;{{{ ispell
 
 (bind-key "C-$"   'ispell-complete-word)
