dcd892b798b092c18952c27b6d2455c838952406
Adam Spiers <dotfiles@adamspiers.org>
2017-10-05 12:37:43 +0100

    move mutt to as-mail.el

--- .emacs.d/init.d/as-comms.el
+++ .emacs.d/init.d/as-comms.el
@@ -1,20 +1,6 @@
 ;; Interaction / communication with other people.
 ;; See as-mail.el for mail-specific setup.
 
-;;{{{ mutt
-
-(autoload 'mutt-mode "mutt" "Mode for editing mutt files")
-(add-to-list 'auto-mode-alist '("/mutt-\\|itsalltext.*mail\\.google" . mail-mode))
-(eval-when-compile (require 'sendmail))
-(add-hook
- 'mail-mode-hook
- (lambda ()
-   (turn-on-auto-fill)
-   (as-setup-mode-for-discussion)
-   (as-set-local-server-edit-keys)))
-
-;;}}}
-
 (defun as-setup-mode-for-discussion ()
   "Sets up a text mode in the way Adam likes for discussion with
 other people."
--- .emacs.d/init.d/as-mail.el
+++ .emacs.d/init.d/as-mail.el
@@ -1,5 +1,15 @@
 ;; See as-comms.el for setup related to general communication
 
+(autoload 'mutt-mode "mutt" "Mode for editing mutt files")
+(add-to-list 'auto-mode-alist '("/mutt-\\|itsalltext.*mail\\.google" . message-mode))
+(eval-when-compile (require 'sendmail))
+(add-hook
+ 'mail-mode-hook
+ (lambda ()
+   (turn-on-auto-fill)
+   (as-setup-mode-for-discussion)
+   (as-set-local-server-edit-keys)))
+
 (autoload 'crm114-mode "crm114-mode" "crm114-mode" t)
 (add-to-list 'auto-mode-alist '("\\.crm$" . crm114-mode))
 
