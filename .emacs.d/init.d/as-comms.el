c04f2980333efc09d2db11c08170f2622896dcad
Adam Spiers <dotfiles@adamspiers.org>
2017-10-05 11:38:20 +0100

    fix indentation

--- .emacs.d/init.d/as-comms.el
+++ .emacs.d/init.d/as-comms.el
@@ -60,10 +60,10 @@
   :requires org
   :config
   (add-hook 'org-mode-hook
-	    (lambda ()
-	      (and (buffer-file-name)
-		   (string-match "\\.o2b$" (buffer-file-name))
-		   (org2blog/wp-mode)))))
+            (lambda ()
+              (and (buffer-file-name)
+                   (string-match "\\.o2b$" (buffer-file-name))
+                   (org2blog/wp-mode)))))
 
 (bind-key "C-c w l"  'org2blog/wp-login)
 (bind-key "C-c w n"  'org2blog/wp-new-entry)
