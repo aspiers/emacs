239d4e60ee256181ca37f496a1b5e01696d9dcd6
Adam Spiers <dotfiles@adamspiers.org>
2018-06-15 17:11:07 +0100

    scrap messages-are-flowing for now

--- .emacs.d/init.d/as-mail.el
+++ .emacs.d/init.d/as-mail.el
@@ -1,9 +1,14 @@
 ;; See as-comms.el for setup related to general communication
 
-(req-package messages-are-flowing
-  :config
-  (add-hook 'message-mode-hook
-            'messages-are-flowing-use-and-mark-hard-newlines))
+;; messages-are-flowing is currently useless outside gnus:
+;; https://github.com/legoscia/messages-are-flowing/issues/2#issuecomment-333989987
+;;
+;; But even if it was, it's really annoying to continually reprompt
+;; https://github.com/legoscia/messages-are-flowing/issues/6
+;; (req-package messages-are-flowing
+;;   :config
+;;   (add-hook 'message-mode-hook
+;;             'messages-are-flowing-use-and-mark-hard-newlines))
 
 (autoload 'mutt-mode "mutt" "Mode for editing mutt files")
 (add-to-list 'auto-mode-alist '("/mutt-\\|itsalltext.*mail\\.google" . message-mode))
