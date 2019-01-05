65779d36af9d1e950a5536e2a9037f47baa1240b
Adam Spiers <dotfiles@adamspiers.org>
2019-01-05 13:03:06 +0000

    speed up quelpa initialisation

--- .emacs.d/init.d/as-quelpa.el
+++ .emacs.d/init.d/as-quelpa.el
@@ -1,13 +1,15 @@
 (if (require 'quelpa nil t)
-    ;; Skipping buggy quelpa-self-upgrade
+    (as-progress "Skipping buggy quelpa-self-upgrade")
     ;;(quelpa-self-upgrade)
   (with-temp-buffer
     (url-insert-file-contents "https://raw.github.com/quelpa/quelpa/master/bootstrap.el")
     (eval-buffer)))
 
+(as-progress "Setting up quelpa-use-package")
 ;; install use-package and the quelpa handler
 (quelpa '(quelpa-use-package :fetcher github :repo "quelpa/quelpa-use-package"))
 
+(as-progress "Requiring quelpa-use-package")
 (require 'quelpa-use-package)
 
 (provide 'as-quelpa)
--- lib/emacs/init/GNU_Emacs/as-custom-26.1.el
+++ lib/emacs/init/GNU_Emacs/as-custom-26.1.el
@@ -2146,7 +2146,8 @@ Reply-To: Adam Spiers <usenet@adamspiers.org>
  '(ps-lpr-command "kprinter")
  '(ps-paper-type (quote a4))
  '(ps-print-color-p (quote black-white))
- '(quelpa-upgrade-p t)
+ '(quelpa-update-melpa-p nil)
+ '(quelpa-upgrade-p nil)
  '(region-bindings-mode-disable-predicates (quote ((lambda nil buffer-read-only))))
  '(region-bindings-mode-enabled-modes
    (quote
