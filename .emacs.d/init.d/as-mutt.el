3170b2127a7b6a1276bd8e4102b2cdbefd1f107e
Adam Spiers <dotfiles@adamspiers.org>
2019-01-07 18:24:02 +0000

    some progress towards fixing muttrc-mode

--- .emacs.d/init.d/as-mutt.el
+++ .emacs.d/init.d/as-mutt.el
@@ -1,3 +1,9 @@
-(req-package muttrc-mode)
+;; quelpa can't install this right now :-(
+;; https://framagit.org/steckerhalter/quelpa/issues/161
+
+;; (req-package muttrc-mode
+;;   :quelpa (muttrc-mode
+;;            :fetcher github
+;;            :repo "neomutt/muttrc-mode-el"))
 
 (provide 'as-mutt)
