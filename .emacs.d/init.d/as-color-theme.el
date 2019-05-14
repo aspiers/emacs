4887fa7130f5b255cf98aff493dab180e0e65629
Adam Spiers <dotfiles@adamspiers.org>
2019-05-14 14:30:37 +0100

    fix theme loading

--- .emacs.d/init.d/as-color-theme.el
+++ .emacs.d/init.d/as-color-theme.el
@@ -4,8 +4,17 @@
     '(("zenburn-bg" . "#000000")))
   (load-theme 'zenburn t))
 
-(req-package sunlight-theme
-  :no-require t
-  :ensure nil)
+(add-to-list 'custom-theme-load-path as-themes-dir)
+
+;; These are not needed as long as the themes directory is in
+;; custom-theme-load-path:
+;;
+;; (req-package pastels-on-dark-aspiers-theme
+;;   :no-require t
+;;   :ensure nil)
+;; 
+;; (req-package sunlight-theme
+;;   :no-require t
+;;   :ensure nil)
 
 (provide 'as-color-theme)
