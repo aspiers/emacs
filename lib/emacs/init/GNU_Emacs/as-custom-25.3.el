fddb0f5acba239ad965f8b39f1645fcd1cdbd546
Adam Spiers <dotfiles@adamspiers.org>
2020-01-05 13:27:49 +0000

    add empty custom-set-faces to 25.3

--- lib/emacs/init/GNU_Emacs/as-custom-25.3.el
+++ lib/emacs/init/GNU_Emacs/as-custom-25.3.el
@@ -2253,3 +2253,9 @@ Reply-To: Adam Spiers <usenet@adamspiers.org>
  '(yas-snippet-dirs
    (quote
     ("~/lib/emacs/minor-modes/yasnippet/snippets" "~/lib/emacs/minor-modes/yasnippets-rails/rails-snippets"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
