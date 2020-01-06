3cbc9fecfe37997dbfaa7973cc7b147f85060c24
Adam Spiers <dotfiles@adamspiers.org>
2020-01-06 15:53:13 +0000

    magit-hash face colour

--- lib/emacs/init/GNU_Emacs/as-custom-26.1.el
+++ lib/emacs/init/GNU_Emacs/as-custom-26.1.el
@@ -1403,4 +1403,4 @@ Reply-To: Adam Spiers <usenet@adamspiers.org>
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(magit-hash ((t (:foreground "grey50")))))
