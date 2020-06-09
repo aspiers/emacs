540bce654fe6cc57beacbc5bbe22af1fc3223b85
Adam Spiers <dotfiles@adamspiers.org>
2020-06-09 13:07:28 +0100

    Add org-link-from-user-regexp

--- lib/emacs/init/GNU_Emacs/as-custom-26.3.el
+++ lib/emacs/init/GNU_Emacs/as-custom-26.3.el
@@ -1053,6 +1053,8 @@ Reply-To: Adam Spiers <usenet@adamspiers.org>
     ((vm . vm-visit-folder-other-frame)
      (gnus . gnus-other-frame)
      (file . find-file))))
+ '(org-link-from-user-regexp
+   "\\<\\(adam@spiers\\.net\\|Adam Spiers\\|@\\(adamspiers\\|tigerpig\\)\\.org\\|aspiers@\\(novell\\|suse\\)\\.com\\)\\>")
  '(org-log-done (quote time))
  '(org-lowest-priority 69)
  '(org-mairix-augmented-links nil)
