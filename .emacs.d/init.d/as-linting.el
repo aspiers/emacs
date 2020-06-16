c3c6c3ee3f42d319acb371929c4c910929dd31d2
Adam Spiers <dotfiles@adamspiers.org>
2020-06-16 22:02:33 +0100

    Fix loading of prettier.el

--- .emacs.d/init.d/as-linting.el
+++ .emacs.d/init.d/as-linting.el
@@ -1,6 +1,11 @@
 (defvar prettier-el-home "~/.GIT/3rd-party/prettier.el")
 
 (if (file-directory-p prettier-el-home)
+    ;; We have to ensure the dependency because we're not using the
+    ;; normal package-install mechanism here :-/  straight.el would
+    ;; probably fix this.
+    (use-package iter2)
+
     (use-package prettier
       :load-path prettier-el-home)
   (warn "%s did not exist; won't install prettier.el"))
