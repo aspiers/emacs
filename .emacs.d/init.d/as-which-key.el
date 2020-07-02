c4d92299340c730c7414a975f6d85cfd0ce57b50
Adam Spiers <dotfiles@adamspiers.org>
2020-07-02 02:49:58 +0100

    substantially improve which-key mode

--- .emacs.d/init.d/as-which-key.el
+++ .emacs.d/init.d/as-which-key.el
@@ -1,16 +1,29 @@
+(defvar as-which-key-no-delay-prefixes ()
+  "List of regexps matching keymap prefix bindings for which
+`which-key` should not delay showing the keymap's bindings.")
+
 (use-package which-key
-  :defer 10
+  :defer 0.1
+  :diminish WK  ;; can't get this working
   :config
-  (which-key-mode)
+
+  ;; These have to go before the mode is activated
   (setq which-key-idle-delay 0.1)
+  (setq which-key-idle-secondary-delay 0.1)
+  ;; FIXME: This one doesn't work >-(
+  (setq which-key-enable-extended-define-key t)
+  (setq which-key-lighter "")
+
+  (which-key-mode)
+
+  (require 's)
   (defun as-which-key-delay-function (prefix length)
-    (cond ((string-match-p
-            "C-c j\\|<key-chord> z j"
-            prefix)
+    (cond ((and as-which-key-no-delay-prefixes
+                (string-match-p
+                 (s-join "\\|" as-which-key-no-delay-prefixes)
+                 prefix))
            0)
           (t 1.0)))
   (add-to-list 'which-key-delay-functions 'as-which-key-delay-function))
-
-(setq which-key-enable-extended-define-key t)
 
 (provide 'as-which-key)
