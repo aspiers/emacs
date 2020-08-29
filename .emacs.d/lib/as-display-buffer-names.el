edecaf98be71e37eb22d080079990331f0355ed5
Adam Spiers <emacs@adamspiers.org>
2020-08-29 19:13:53 +0100

    Make as-display-buffer-filename work with Info buffers too

--- .emacs.d/lib/as-display-buffer-names.el
+++ .emacs.d/lib/as-display-buffer-names.el
@@ -12,19 +12,24 @@ and in the X selection for other programs.  If the prefix argument
 is 2, forks the external program abs (must be in $PATH) to convert
 the filename to an absolute one with all symlinks resolved."
   (interactive "P")
-  (or buffer-file-name (error "No file associated with this buffer"))
-  (let ((fn (cond ((and save-to-clipboard (eq save-to-clipboard 2))
-                   (substring
-                    (shell-command-to-string (concat "abs " buffer-file-name))
-                    0 -1))
-                  (t buffer-file-name))))
-    (cond (save-to-clipboard
-           (kill-new fn)
-;; No need for x-select-text since kill-new calls function set
-;; in interprogram-cut-function, which is x-select-text anyway.
-;;         (x-select-text buffer-file-name)
-           (setq fn (concat fn " (stored in kill ring and X selection)"))))
-    (message fn)))
+  (let ((file-name
+         (cond ((eq major-mode 'Info-mode)
+                Info-current-file)
+               (buffer-file-name)
+               (t
+                (error "No file associated with this buffer")))))
+    (let ((fn (cond ((and save-to-clipboard (eq save-to-clipboard 2))
+                     (substring
+                      (shell-command-to-string (concat "abs " file-name))
+                      0 -1))
+                    (t file-name))))
+      (cond (save-to-clipboard
+             (kill-new fn)
+             ;; No need for x-select-text since kill-new calls function set
+             ;; in interprogram-cut-function, which is x-select-text anyway.
+             ;;         (x-select-text buffer-file-name)
+             (setq fn (concat fn " (stored in kill ring and X selection)"))))
+      (message fn))))
 
 ;;}}}
 ;;{{{ as-display-buffer-name
