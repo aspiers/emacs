3f0e077dad4f9b8057a0624dda0cafc76398d7a9
Adam Spiers <emacs@adamspiers.org>
2020-09-05 23:36:02 +0100

    Switch mutt-mode to use-package

--- .emacs.d/init.d/as-mail.el
+++ .emacs.d/init.d/as-mail.el
@@ -14,17 +14,14 @@
 (use-feature flowed-text
   :bind ("M-s M-m" . format-as-flowed-text))
 
-(autoload 'mutt-mode "mutt" "Mode for editing mutt files")
-(add-to-list 'auto-mode-alist '("/mutt-\\|itsalltext.*mail\\.google" . mail-mode))
-(eval-when-compile (require 'sendmail))
-(add-hook
- 'mail-mode-hook
- (lambda ()
-   (turn-on-auto-fill)
-   (as-setup-mode-for-discussion)
-   (as-set-local-server-edit-keys)))
-
-(autoload 'crm114-mode "crm114-mode" "crm114-mode" t)
-(add-to-list 'auto-mode-alist '("\\.crm$" . crm114-mode))
+(use-package mutt-mode
+  :mode ("/mutt-\\|itsalltext.*mail\\.google" . mail-mode)
+  :config
+  (require 'sendmail)
+  :hook (mail-mode-hook
+         . (lambda ()
+             (turn-on-auto-fill)
+             (as-setup-mode-for-discussion)
+             (as-set-local-server-edit-keys))))
 
 (provide 'as-mail)
