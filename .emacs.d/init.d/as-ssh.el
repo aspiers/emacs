88903f6bcd30d5ac124af8d10c04bd71ccc7a178
Adam Spiers <dotfiles@adamspiers.org>
2020-10-15 21:51:46 +0100

    add as-read-ssh-agent-cache

---
+++ .emacs.d/init.d/as-ssh.el
@@ -0,0 +1,11 @@
+(defun as-read-ssh-agent-cache ()
+  (interactive)
+  (let* ((cache-file (format "/tmp/.dsa-cache-adamspiers-%s.el" (system-name))))
+   (if (not (file-exists-p cache-file))
+       (warn "Couldn't find %s" cache-file)
+     (load cache-file)
+     (message "Using ssh-agent with pid %s" (getenv "SSH_AGENT_PID")))))
+
+(as-read-ssh-agent-cache)
+
+(provide 'as-ssh)
