4cca178a1ae1453ce310b49cdb70f94b31b09460
Adam Spiers <emacs@adamspiers.org>
2020-11-30 00:43:06 +0000

    use diminish utility

--- .emacs.d/init.d/as-mode-lighters.el
+++ .emacs.d/init.d/as-mode-lighters.el
@@ -1,17 +1,3 @@
-;; http://hbin.me/blog/2012/08/26/clean-the-emacs-mode-line/
-
-;; obsoleted by rich-minority which smart-mode-line honours
-
-;; (defun hbin-remove-mm-lighter (mm)
-;;   "Remove minor lighter from the mode line."
-;;   (let ((lighter (assq mm minor-mode-alist)))
-;;     (when lighter
-;;       (setcar (cdr lighter) nil))))
-;; 
-;; (hbin-remove-mm-lighter 'global-whitespace-mode)
-;; (hbin-remove-mm-lighter 'whitespace-mode)
-;; (hbin-remove-mm-lighter 'guide-key-mode)
-;; (hbin-remove-mm-lighter 'smartparens-mode)
-;; (hbin-remove-mm-lighter 'projectile-mode)
+(use-package diminish)
 
 (provide 'as-mode-lighters)
