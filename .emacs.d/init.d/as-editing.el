14c80087870f916548a330f3d129533a8c01aa84
Adam Spiers <emacs@adamspiers.org>
2020-12-15 22:18:15 +0000

    remove redundant bind of C-M-k

--- .emacs.d/init.d/as-editing.el
+++ .emacs.d/init.d/as-editing.el
@@ -21,7 +21,6 @@ The list marked is the one that contains point or follows point."
          ("M-D"   . as-kill-word)
          ("C-M-S-k" . as-kill-sexp)))
 
-(bind-key "C-M-k"   'kill-sexp)
 ;; https://karthinks.com/software/batteries-included-with-emacs/#cycle-spacing--m-spc
 (bind-key "M-SPC"   'cycle-spacing)
 ;; (bind-key "M-\\"    'fixup-whitespace)
