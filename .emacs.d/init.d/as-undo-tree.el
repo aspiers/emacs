bc87b70d6200a3c2c403173fa0096c7c52e06dd3
Adam Spiers <emacs@adamspiers.org>
2021-01-24 11:53:03 +0000

    undo-tree has cool timestamps

--- .emacs.d/init.d/as-undo-tree.el
+++ .emacs.d/init.d/as-undo-tree.el
@@ -1,7 +1,8 @@
 (require 'as-el-get)
 
-(req-package undo-tree
+(use-package undo-tree
   :init
-  (global-undo-tree-mode))
+  (global-undo-tree-mode)
+  (setq undo-tree-visualizer-timestamps t))
 
 (provide 'as-undo-tree)
