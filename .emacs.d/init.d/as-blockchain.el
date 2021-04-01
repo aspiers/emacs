40d7ea377fb8ea7a62f597020a5f31bde3ce0b9a
Adam Spiers <emacs@adamspiers.org>
2021-04-01 14:32:41 +0100

    Improve solidity config

--- .emacs.d/init.d/as-blockchain.el
+++ .emacs.d/init.d/as-blockchain.el
@@ -1,4 +1,22 @@
-(use-package solidity-mode)
-(use-package flymake-solidity)
+;; Lefteris FTW! https://github.com/ethereum/emacs-solidity/
+(use-package solidity-mode
+  :config
+  (setq solidity-comment-style 'slash))
+
+(use-package solidity-flycheck
+  :config
+  (setq solidity-flycheck-solc-checker-active t)
+  (setq solidity-flycheck-solium-checker-active t)
+  ;; (setq solidity-flycheck-chaining-error-level ...)
+  )
+
+(with-packages (solidity-mode solidity-flycheck)
+  :config
+  (add-hook 'solidity-mode-hook 'flycheck-mode))
+
+(use-package company-solidity)
+
+;; Unmaintained: https://github.com/kootenpv/flymake-solidity/tree/master
+;; (use-package flymake-solidity)
 
 (provide 'as-blockchain)
