6182de2ba2e2be62282f34671bfab0e967affa2c
Adam Spiers <emacs@adamspiers.org>
2021-05-23 15:28:05 +0100

    Use nice assq-delete-all trick from @basil-conto
    https://github.com/abo-abo/swiper/issues/2751#issuecomment-846205524

--- .emacs.d/init.d/as-selection.el
+++ .emacs.d/init.d/as-selection.el
@@ -95,11 +95,9 @@ has built the current list of completion matches."
   (add-to-list 'ivy-sort-matches-functions-alist
                '(set-any-variable . ivy--prefix-sort)
                'append)
-  (let ((existing (assoc 'ivy-switch-buffer
-                         ivy-sort-matches-functions-alist)))
-    (when existing
-      (setq ivy-sort-matches-functions-alist
-            (delete existing ivy-sort-matches-functions-alist))))
+  (setq ivy-sort-matches-functions-alist
+        (assq-delete-all #'ivy-switch-buffer
+                         ivy-sort-matches-functions-alist))
 
   (ivy--alist-set 'ivy-initial-inputs-alist 'org-refile "")
   (ivy--alist-set 'ivy-initial-inputs-alist 'org-agenda-refile "")
