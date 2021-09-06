604f6ef2d69264781b1e77e266a5508c0cbee0d3
Adam Spiers <dotfiles@adamspiers.org>
2021-09-06 18:22:12 +0100

    Add comment about merlin-company

--- .emacs.d/init.d/as-ocaml.el
+++ .emacs.d/init.d/as-ocaml.el
@@ -1,7 +1,10 @@
 (use-package tuareg
   :mode ("\\.ml\\'" . tuareg-mode))
 
+;; If this causes issues with merlin-company, make sure merlin is up
+;; to date.
 (use-package flycheck-ocaml)
+
 (use-package ocp-indent)
 
 (provide 'as-ocaml)
