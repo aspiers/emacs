1598be396040e9e9f7f1f7b1d0c958c6684ab6cf
Adam Spiers <emacs@adamspiers.org>
2021-09-08 01:55:18 +0100

    Upgrade lsp installation

--- .emacs.d/init.d/as-lsp.el
+++ .emacs.d/init.d/as-lsp.el
@@ -1,7 +1,32 @@
 ;; https://emacs-lsp.github.io/lsp-mode/page/installation/
 (use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "M-L")
+
   :hook ((python-mode . lsp)
          (js-mode . lsp)
-         (typescript-mode . lsp)))
+         (typescript-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+
+  :commands lsp)
+
+;; optionally
+(use-package lsp-ui :commands lsp-ui-mode)
+;; if you are helm user
+(use-package helm-lsp :commands helm-lsp-workspace-symbol)
+;; if you are ivy user
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+;; optionally if you want to use debugger
+(use-package dap-mode)
+;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+
+;; optional if you want which-key integration
+(use-package which-key
+    :config
+    (which-key-mode))
 
 (provide 'as-lsp)
