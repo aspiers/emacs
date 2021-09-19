5539e2fd0217c82bcf993ce326239bebc7c6fb9f
Adam Spiers <emacs@adamspiers.org>
2021-09-19 21:45:06 +0100

    Add json-mode

--- .emacs.d/init.d/as-tech-langs.el
+++ .emacs.d/init.d/as-tech-langs.el
@@ -10,4 +10,12 @@
 (use-package yaml-mode
   :mode ("\\.ya?ml\\'" . yaml-mode))
 
+(use-package json-mode
+  ;; https://github.com/kiennq/json-mode is currently ahead of
+  ;; the upstream https://github.com/joshwnj/json-mode, and it
+  ;; seems the upstream may be dead:
+  ;; https://github.com/joshwnj/json-mode/issues/64
+  :straight (:host github :repo "kiennq/json-mode")
+  :mode ("\\.json\\'" . json-mode))
+
 (provide 'as-tech-langs)
