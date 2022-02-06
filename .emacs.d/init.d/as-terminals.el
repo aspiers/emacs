75d096600b219faef73b956d2234f3c2d9257dc2
Adam Spiers <emacs@adamspiers.org>
2022-02-06 22:17:49 +0000

    more vterm shortcuts

--- .emacs.d/init.d/as-terminals.el
+++ .emacs.d/init.d/as-terminals.el
@@ -1,6 +1,8 @@
 (use-package vterm)
 
 (use-package vterm-toggle
-  :bind ("C-`" . vterm-toggle))
+  :bind (("C-`" . vterm-toggle)
+         ("M-`" . vterm-toggle-forward)
+         ("M-~" . vterm-toggle-backward)))
 
 (provide 'as-terminals)
