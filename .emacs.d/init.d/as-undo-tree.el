10851086bc5893efb1606fcea55bb3ff032da30d
Adam Spiers <emacs@adamspiers.org>
2022-04-05 22:25:03 +0100

    undo-tree-enable-undo-in-region => t
    https://irreal.org/blog/?p=8614

--- .emacs.d/init.d/as-undo-tree.el
+++ .emacs.d/init.d/as-undo-tree.el
@@ -3,6 +3,7 @@
 (use-package undo-tree
   :init
   (global-undo-tree-mode)
-  (setq undo-tree-visualizer-timestamps t))
+  (setq undo-tree-visualizer-timestamps t
+        undo-tree-enable-undo-in-region t))
 
 (provide 'as-undo-tree)
