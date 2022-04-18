010702b5fdf854b3cbb5b72bc2163a0746210d8f
Adam Spiers <dotfiles@adamspiers.org>
2022-04-18 22:21:12 +0200

    telega

--- .emacs.d/init.d/as-comms.el
+++ .emacs.d/init.d/as-comms.el
@@ -17,4 +17,8 @@ other people."
 (bind-key "C-$"   'ispell-complete-word)
 (bind-key "C-M-$" 'ispell-buffer)
 
+;; Telegram client for emacs!
+;; https://zevlg.github.io/telega.el/
+;; (use-package telega)
+
 (provide 'as-comms)
