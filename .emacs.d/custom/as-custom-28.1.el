0bd005a979731e00cad83dfbef3b333ca954e4c0
Adam Spiers <dotfiles@adamspiers.org>
2022-04-25 00:30:36 +0200

    remove htmltidy

--- .emacs.d/custom/as-custom-28.1.el
+++ .emacs.d/custom/as-custom-28.1.el
@@ -1172,7 +1172,6 @@ Reply-To: Adam Spiers <usenet@adamspiers.org>
  '(speedbar-tag-split-minimum-length 30)
  '(tide-always-show-documentation t)
  '(tide-completion-detailed t)
- '(tidy-shell-command "htmltidy")
  '(tla-non-recursive-inventory nil)
  '(tool-bar-mode nil)
  '(tool-bar-position 'top)
