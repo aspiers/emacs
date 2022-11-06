4fc9d72df8ce15c15a1d18c7664b2bc93e453731
Adam Spiers <emacs@adamspiers.org>
2022-11-06 19:24:42 +0000

    fail to get LSP working with Golang

--- .emacs.d/init.d/as-golang.el
+++ .emacs.d/init.d/as-golang.el
@@ -1,4 +1,20 @@
 (use-package go-mode)
+
+(with-packages (go-mode lsp-mode)
+  :hook (go-mode . lsp-deferred)
+  :config
+  (lsp-register-custom-settings
+   '(("gopls.completeUnimported" t t)
+     ("gopls.staticcheck" t t))))
+
+;; https://github.com/golang/tools/blob/master/gopls/doc/emacs.md
+;; suggests:
+;;
+;;  (defun lsp-go-install-save-hooks ()
+;;   (add-hook 'before-save-hook #'lsp-format-buffer t t)
+;;   (add-hook 'before-save-hook #'lsp-organize-imports t t))
+;; (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
 (use-package go-gopath)
 
 (provide 'as-golang)
