1a3cd1f11220535897dff37bde5c414463dd94e9
Adam Spiers <dotfiles@adamspiers.org>
2023-01-02 22:49:46 +0000

    try a more modern approach to setting the default font

--- .emacs.d/init.d/as-fonts.el
+++ .emacs.d/init.d/as-fonts.el
@@ -3,4 +3,9 @@
   :if (<= emacs-major-version 27)
   :config (unicode-fonts-setup))
 
+(set-face-attribute 'default nil :height 140)
+;; This seems to do the same
+;; (set-frame-font "14" nil t)
+;; (set-frame-font "Source Code Pro 14" nil t)
+
 (provide 'as-fonts)
