bb1b83150d9e7d0ef847b56005ee68fa3aa264b9
Adam Spiers <emacs@adamspiers.org>
2023-03-28 00:04:18 +0100

    add link to article about setting the default emacs font

--- .emacs.d/init.d/as-fonts.el
+++ .emacs.d/init.d/as-fonts.el
@@ -3,6 +3,7 @@
   :if (<= emacs-major-version 27)
   :config (unicode-fonts-setup))
 
+;; See also https://emacsredux.com/blog/2023/03/16/setting-the-default-font-for-emacs/
 (set-face-attribute 'default nil :height 140)
 ;; This seems to do the same
 ;; (set-frame-font "14" nil t)
