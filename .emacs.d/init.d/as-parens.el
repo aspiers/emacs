16671636de1ed657c2e8b058eaac10a5aece9fe3
Adam Spiers <dotfiles@adamspiers.org>
2023-06-04 19:23:20 +0100

    as-parens: add sp-ignore-modes-list with emacs-lisp-mode
    Move from as-custom

--- .emacs.d/init.d/as-parens.el
+++ .emacs.d/init.d/as-parens.el
@@ -1,6 +1,11 @@
 (require 'paren)
 
-(use-package smartparens)
+(use-package smartparens
+  :config
+  (setq sp-ignore-modes-list
+        '(minibuffer-mode
+          minibuffer-inactive-mode
+          emacs-lisp-mode)))
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
