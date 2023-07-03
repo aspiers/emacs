9c84a07d632768a62a2905f69d195552e2c80400
Adam Spiers <emacs@adamspiers.org>
2023-07-03 11:48:15 +0100

    enable (show-)smartparens globally

--- .emacs.d/init.d/as-parens.el
+++ .emacs.d/init.d/as-parens.el
@@ -5,7 +5,9 @@
   (setq sp-ignore-modes-list
         '(minibuffer-mode
           minibuffer-inactive-mode
-          emacs-lisp-mode)))
+          emacs-lisp-mode))
+  (smartparens-global-mode t)
+  (show-smartparens-global-mode t))
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
