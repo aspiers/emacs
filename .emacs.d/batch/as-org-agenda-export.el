75fdd7e037f7f36f8a61c0b4c80ae522f4fa79bd
Adam Spiers <emacs@adamspiers.org>
2023-07-03 11:49:01 +0100

    parametrise as-org-agenda-export

--- .emacs.d/batch/as-org-agenda-export.el
+++ .emacs.d/batch/as-org-agenda-export.el
@@ -1,9 +1,16 @@
+(add-to-list 'load-path
+             (concat (getenv "HOME")
+                     "/.emacs.d/straight/repos/org/lisp"))
+
+(require 'org)
 (require 'ox-icalendar)
 
-(let ((org-agenda-files
-       '("~/blockchain/Toucan/TODO.org"
-         "/home/adam/org/TODO.org"
-         "/home/adam/Panther/TODO.org"))
+(load (concat (getenv "HOME") "/.as-custom.el"))
+
+(let ((org-agenda-files argv)
       (org-icalendar-store-UID nil)
       (org-export-with-broken-links t))
-  (org-icalendar-export-agenda-files))
+  (org-icalendar-export-agenda-files)
+  ;; (org-icalendar-combine-agenda-files)
+  ;; (org-icalendar-export-to-ics)
+  )
