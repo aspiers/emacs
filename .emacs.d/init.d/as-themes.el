e13e99f08e501d998d1206d1a081307266f1e9f4
Adam Spiers <dotfiles@adamspiers.org>
2023-07-12 19:03:04 +0100

    switch to ef-themes but keep modus-themes as an option

--- .emacs.d/init.d/as-themes.el
+++ .emacs.d/init.d/as-themes.el
@@ -9,6 +9,58 @@
 
 (add-to-list 'custom-theme-load-path as-themes-dir)
 
+(use-package ef-themes
+  :config
+  ;; Make customisations that affect Emacs faces BEFORE loading a theme
+  ;; (any change needs a theme re-load to take effect).
+
+  ;; If you like two specific themes and want to switch between them, you
+  ;; can specify them in `ef-themes-to-toggle' and then invoke the command
+  ;; `ef-themes-toggle'.  All the themes are included in the variable
+  ;; `ef-themes-collection'.
+  (setq ef-themes-to-toggle '(ef-summer ef-winter))
+
+  ;; (setq ef-themes-headings ; read the manual's entry or the doc string
+  ;;       '((0 variable-pitch light 1.9)
+  ;;         (1 variable-pitch light 1.8)
+  ;;         (2 variable-pitch regular 1.7)
+  ;;         (3 variable-pitch regular 1.6)
+  ;;         (4 variable-pitch regular 1.5)
+  ;;         (5 variable-pitch 1.4)      ; absence of weight means `bold'
+  ;;         (6 variable-pitch 1.3)
+  ;;         (7 variable-pitch 1.2)
+  ;;         (t variable-pitch 1.1)))
+
+  ;; They are nil by default...
+  (setq ef-themes-mixed-fonts t
+        ef-themes-variable-pitch-ui t)
+
+  ;; Read the doc string or manual for this one.  The symbols can be
+  ;; combined in any order.
+  (setq ef-themes-region '(intense no-extend neutral))
+
+  ;; Disable all other themes to avoid awkward blending:
+  (mapc #'disable-theme custom-enabled-themes)
+
+  ;; Load the theme of choice:
+  ;; (load-theme 'ef-summer :no-confirm)
+
+  ;; OR use this to load the theme which also calls `ef-themes-post-load-hook':
+  (ef-themes-select 'ef-winter))
+
+;; The themes we provide are recorded in the `ef-themes-dark-themes',
+;; `ef-themes-light-themes'.
+
+;; We also provide these commands, but do not assign them to any key:
+;;
+;; - `ef-themes-toggle'
+;; - `ef-themes-select'
+;; - `ef-themes-select-dark'
+;; - `ef-themes-select-light'
+;; - `ef-themes-load-random'
+;; - `ef-themes-preview-colors'
+;; - `ef-themes-preview-colors-current'
+
 (use-package modus-themes
   :config
 
@@ -25,15 +77,21 @@
 (defun as-toggle-bright-theme ()
   "Toggles a bright theme for outdoors vs. indoors."
   (interactive)
-  (let ((dark-theme 'modus-vivendi)
-        (bright-theme 'modus-operandi))
-    ;; or sunlight and pastels-on-dark-aspiers
-    (if (memq bright-theme custom-enabled-themes)
-        (progn
-          (disable-theme bright-theme)
-          (load-theme dark-theme))
-      (disable-theme dark-theme)
-      (load-theme bright-theme))))
+  (cond ((or
+          (memq 'ef-summer custom-enabled-themes)
+          (memq 'ef-winter custom-enabled-themes))
+         (ef-themes-toggle))
+        (t
+         ;; toggle modus-theme
+         (let ((dark-theme 'modus-vivendi)
+               (bright-theme 'modus-operandi))
+           ;; or sunlight and pastels-on-dark-aspiers
+           (if (memq bright-theme custom-enabled-themes)
+               (progn
+                 (disable-theme bright-theme)
+                 (load-theme dark-theme))
+             (disable-theme dark-theme)
+             (load-theme bright-theme))))))
 
 (require 'as-toggles)
 (use-package hydra
