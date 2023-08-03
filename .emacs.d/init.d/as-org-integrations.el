5c8bdf0c7d162b934892a84ea2483c7fce3cd679
Adam Spiers <dotfiles@adamspiers.org>
2023-08-03 17:33:27 +0100

    org-notion-keymap-prefix

--- .emacs.d/init.d/as-org-integrations.el
+++ .emacs.d/init.d/as-org-integrations.el
@@ -2,6 +2,8 @@
 
 (use-package org-notion
   :straight (:host github :repo "richardwesthaver/org-notion")
+  :custom
+  (org-notion-keymap-prefix "C-c M-n")
   :hook (org-mode . org-notion-mode))
 
 (provide 'as-org-integrations)
