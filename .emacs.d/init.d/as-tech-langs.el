56e985ff9f89050711b05403af5f2a3cb68de609
Adam Spiers <dotfiles@adamspiers.org>
2025-06-29 22:35:47 +0100

    switch to modern json-mode repo

--- .emacs.d/init.d/as-tech-langs.el
+++ .emacs.d/init.d/as-tech-langs.el
@@ -11,11 +11,6 @@
   :mode ("\\.ya?ml\\'" . yaml-mode))
 
 (use-package json-mode
-  ;; https://github.com/kiennq/json-mode is currently ahead of
-  ;; the upstream https://github.com/joshwnj/json-mode, and it
-  ;; seems the upstream may be dead:
-  ;; https://github.com/joshwnj/json-mode/issues/64
-  :straight (:host github :repo "kiennq/json-mode")
   :mode ("\\.json\\'" . json-mode))
 
 (provide 'as-tech-langs)
