e7c4b4f83bf27646dc1ba8d184a859a0017c285a
Adam Spiers <dotfiles@adamspiers.org>
2025-06-29 22:36:38 +0100

    switch to nerdfonts

--- .emacs.d/init.d/as-fonts.el
+++ .emacs.d/init.d/as-fonts.el
@@ -4,7 +4,13 @@
   :config (unicode-fonts-setup))
 
 ;; See also https://emacsredux.com/blog/2023/03/16/setting-the-default-font-for-emacs/
-(set-face-attribute 'default nil :height 140)
+;; (set-face-attribute 'default nil :family "Hack")
+(set-face-attribute 'default nil :family "Hack Nerd Font")
+;; (set-face-attribute 'default nil :family "Maple Mono NF")
+;; (set-face-attribute 'default nil :family "SauceCodePro Nerd Font")
+
+(set-face-attribute 'default nil :height 150)
+
 ;; This seems to do the same
 ;; (set-frame-font "14" nil t)
 ;; (set-frame-font "Source Code Pro 14" nil t)
