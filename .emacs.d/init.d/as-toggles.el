6a4b814188f2ca3cebdc4247849a21ab8dede9b8
Adam Spiers <dotfiles@adamspiers.org>
2025-08-06 18:53:44 +0000

    add toggle for word-wrap

--- .emacs.d/init.d/as-toggles.el
+++ .emacs.d/init.d/as-toggles.el
@@ -19,7 +19,8 @@
   _a_ abbrev-mode:       %s(format3 abbrev-mode) ^^^^^^^^^^^^^^     _d_ debug-on-error:    %s(format3 debug-on-error)
   _f_ auto-fill-mode:    %3s(if auto-fill-function \"yes\") ^^^     _S_ truncate-lines:    %s(format3 truncate-lines)
   _o_ overwrite-mode:    %3s(if overwrite-mode \"yes\") ^^^^^^^     _w_ tab-width:         %s(format3 tab-width) ^^^^^^^^^^^^^^^^
-_SPC_ whitespace-mode:   %s(format3 whitespace-mode) ^^^^^^^^^^   _TAB_ indent-tabs-mode:  %s(format3 indent-tabs-mode)
+_SPC_ whitespace-mode:   %s(format3 whitespace-mode) ^^^^^^^^^^     _W_ word-wrap:         %s(format3 word-wrap) ^^^^^^^^^^^^^^^^
+                           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   _TAB_ indent-tabs-mode:  %s(format3 indent-tabs-mode)
 "
     ("a" abbrev-mode nil)
     ("d" toggle-debug-on-error nil)
@@ -28,6 +29,7 @@ _SPC_ whitespace-mode:   %s(format3 whitespace-mode) ^^^^^^^^^^   _TAB_ indent-t
     ("s" toggle-truncate-lines nil)
     ("S" toggle-truncate-lines nil)
     ("w" as-set-tab-width nil)
+    ("W" toggle-word-wrap nil)
     ("TAB" as-toggle-indent-tabs-mode nil)
     ("SPC" whitespace-mode nil)
     ("q" nil "quit"))
