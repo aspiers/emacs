37335969f620bba2a686c912d6549e17b880475f
Adam Spiers <dotfiles@adamspiers.org>
2025-09-01 23:05:00 +0100

    add Hypercerts to org-agenda

--- .emacs.d/custom/as-custom-30.1.el
+++ .emacs.d/custom/as-custom-30.1.el
@@ -481,7 +481,8 @@
  '(org-adapt-indentation t)
  '(org-agenda-columns-add-appointments-to-effort-sum t)
  '(org-agenda-files
-   '("/home/adam/org/TODO.org" "/home/adam/blockchain/Toucan/TODO.org"))
+   '("/home/adam/org/TODO.org" "/home/adam/blockchain/Toucan/TODO.org"
+     "/home/adam/org/notes/Hypercerts.org"))
  '(org-agenda-fontify-priorities '((65 (:bold t :weight bold))))
  '(org-agenda-include-deadlines t)
  '(org-agenda-include-diary t)
