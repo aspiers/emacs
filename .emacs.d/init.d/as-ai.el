0c9865f751e6dca56958982c1657ce3a54127508
Adam Spiers <dotfiles@adamspiers.org>
2026-01-27 22:55:49 +1300

    add as-ai

---
+++ .emacs.d/init.d/as-ai.el
@@ -0,0 +1,13 @@
+(straight-use-package
+ '(beads :type git :host github :repo "ChristianTietze/beads.el"
+         :files ("lisp/*.el")
+         :bind ("C-S-b" . beads)))
+
+(use-package claude-code-ide
+  :straight (:type git :host github :repo "manzaltu/claude-code-ide.el")
+  :bind ("C-c C-'" . claude-code-ide-menu) ; Set your favorite keybinding
+  :config
+  (claude-code-ide-emacs-tools-setup)) ; Optionally enable Emacs MCP
+                                       ; tools
+
+(provide 'as-ai)
